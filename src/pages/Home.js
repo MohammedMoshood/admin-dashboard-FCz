6c0ff4d354a37b1145dd471a50fe9ec7d668d3f3
MohammedMoshood <moshood.mm8@gmail.com>
2022-08-04 11:45:11 +0100

    new react template added

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default Home
